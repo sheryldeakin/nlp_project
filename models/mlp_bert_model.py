773aa041224426cdbb2f702d09b0709684d16fba
Henry Rothenberg <hsr205@aol.com>
2025-10-08 15:35:04 -0400

    Refactor _evaluate_ml_model method in mlp_bert_model.py module

--- models/mlp_bert_model.py
+++ models/mlp_bert_model.py
@@ -49,54 +49,56 @@ class MLPBert:
 
         return train_loader, test_loader
 
+    def _evaluate(self, model, loader, device, split_name, label_names):
+        model.eval()
+        all_preds = []
+        all_labels = []
+
+        with torch.no_grad():
+            for X_batch, y_batch in loader:
+                X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+                logits = model(X_batch)
+                probs = torch.sigmoid(logits)  # Convert to probabilities
+                preds = (probs > 0.5).int().cpu().numpy()
+                all_preds.extend(preds)
+                all_labels.extend(y_batch.cpu().numpy())
+
+        # Compute final accuracy & F1-Score for multiclass classification
+        mlp_accuracy = accuracy_score(all_labels, all_preds)
+        mlp_f1_micro = f1_score(all_labels, all_preds, average="micro")
+        mlp_f1_macro = f1_score(all_labels, all_preds, average="macro")
+
+        self.logger.info(f"\n{split_name} Set Metrics for MLP + BERT:")
+        self.logger.info(f"\nOverall Accuracy: {mlp_accuracy:.4f}")
+        self.logger.info(f"Overall F1 Score (Micro): {mlp_f1_micro:.4f}")
+        self.logger.info(f"Overall F1 Score (Macro): {mlp_f1_macro:.4f}")
+
+        y_true = np.array(all_labels)
+        y_pred = np.array(all_preds)
+        # Per-class precision, recall, F1
+        precisions, recalls, f1s, _ = precision_recall_fscore_support(
+            y_true, y_pred, average=None, zero_division=0
+        )
+
+        if isinstance(label_names, (list, np.ndarray)):
+            emotion_labels = label_names.tolist() if isinstance(label_names, np.ndarray) else label_names
+        else:
+            raise ValueError(
+                f"Expected label_names to be a list or ndarray, but got: {type(label_names).__name__}, label_names: {label_names}")
+
+        self.logger.info("\nPer-class Precision / Recall / F1:")
+        for idx, label in enumerate(emotion_labels):  # or custom label list
+            self.logger.info(f"{label:20s} | P: {precisions[idx]:.2f} | R: {recalls[idx]:.2f} | F1: {f1s[idx]:.2f}")
+
+        return mlp_f1_micro, mlp_f1_macro
+
     def _evaluate_mlp_model(self, model, train_loader, test_loader, device, label_names):
-        # Evaluate Model on Test Set
-        def _evaluate(loader, split_name, label_names):
-            model.eval()
-            all_preds = []
-            all_labels = []
-
-            with torch.no_grad():
-                for X_batch, y_batch in loader:
-                    X_batch, y_batch = X_batch.to(device), y_batch.to(device)
-                    logits = model(X_batch)
-                    probs = torch.sigmoid(logits)  # Convert to probabilities
-                    preds = (probs > 0.5).int().cpu().numpy()
-                    all_preds.extend(preds)
-                    all_labels.extend(y_batch.cpu().numpy())
-
-            # Compute final accuracy & F1-Score for multiclass classification
-            mlp_accuracy = accuracy_score(all_labels, all_preds)
-            mlp_f1_micro = f1_score(all_labels, all_preds, average="micro")
-            mlp_f1_macro = f1_score(all_labels, all_preds, average="macro")
-
-            self.logger.info(f"\n{split_name} Set Metrics for MLP + BERT:")
-            self.logger.info(f"\nOverall Accuracy: {mlp_accuracy:.4f}")
-            self.logger.info(f"Overall F1 Score (Micro): {mlp_f1_micro:.4f}")
-            self.logger.info(f"Overall F1 Score (Macro): {mlp_f1_macro:.4f}")
-
-            y_true = np.array(all_labels)
-            y_pred = np.array(all_preds)
-            # Per-class precision, recall, F1
-            precisions, recalls, f1s, _ = precision_recall_fscore_support(
-                y_true, y_pred, average=None, zero_division=0
-            )
-
-            if isinstance(label_names, (list, np.ndarray)):
-                emotion_labels = label_names.tolist() if isinstance(label_names, np.ndarray) else label_names
-            else:
-                raise ValueError(
-                    f"Expected label_names to be a list or ndarray, but got: {type(label_names).__name__}, label_names: {label_names}")
-
-            self.logger.info("\nPer-class Precision / Recall / F1:")
-            for idx, label in enumerate(emotion_labels):  # or custom label list
-                self.logger.info(f"{label:20s} | P: {precisions[idx]:.2f} | R: {recalls[idx]:.2f} | F1: {f1s[idx]:.2f}")
-
-            return mlp_f1_micro, mlp_f1_macro
 
         # Evaluate both train and test
-        mlp_f1_micro_train, mlp_f1_macro_train = _evaluate(train_loader, "Train", label_names)
-        mlp_f1_micro_test, mlp_f1_macro_test = _evaluate(test_loader, "Test", label_names)
+        mlp_f1_micro_train, mlp_f1_macro_train = self._evaluate(model=model, device=device, loader=train_loader,
+                                                                split_name="Train", label_names=label_names)
+        mlp_f1_micro_test, mlp_f1_macro_test = self._evaluate(model=model, device=device, loader=test_loader,
+                                                              split_name="Test", label_names=label_names)
 
         return mlp_f1_micro_train, mlp_f1_macro_train, mlp_f1_micro_test, mlp_f1_macro_test
 
