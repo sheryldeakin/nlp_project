7a17ac83bbb40d8c7c60c9d278f6844038958835
Henry Rothenberg <hsr205@aol.com>
2025-10-08 14:58:15 -0400

    Add demo logic to main.py module

--- controller/main.py
+++ controller/main.py
@@ -1,5 +1,13 @@
+from nlp_project.utils.logger import Logger
+
+
 def main() -> int:
-    print(f"Hello from {__name__}")
+    logger: Logger = Logger(class_name=__name__)
+
+    logger.info(f"Hello from {__name__}")
+    logger.warning(f"Hello from {__name__}")
+    logger.error(f"Hello from {__name__}")
+
     return 0
 
 
