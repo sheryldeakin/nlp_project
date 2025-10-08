0fb742db3a44d09e41203ee880875a96190374f9
Henry Rothenberg <hsr205@aol.com>
2025-10-08 15:41:46 -0400

    Add try-except block to main.py module

--- controller/main.py
+++ controller/main.py
@@ -4,9 +4,14 @@ from nlp_project.utils.logger import Logger
 def main() -> int:
     logger: Logger = Logger(class_name=__name__)
 
-    logger.info(f"Hello from {__name__}")
-    logger.warning(f"Hello from {__name__}")
-    logger.error(f"Hello from {__name__}")
+    try:
+
+        logger.info(f"Hello from {__name__}")
+        logger.warning(f"Hello from {__name__}")
+        logger.error(f"Hello from {__name__}")
+
+    except Exception as e:
+        raise Exception(f"Error Message: {e}")
 
     return 0
 
