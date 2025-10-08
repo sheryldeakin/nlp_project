878c435746cd18b531e40887ae4a72435b82ca81
Henry Rothenberg <hsr205@aol.com>
2025-10-08 16:08:08 -0400

    Add cli_argument parsing / alter imports

--- controller/main.py
+++ controller/main.py
@@ -1,14 +1,19 @@
-from nlp_project.utils.logger import Logger
+from controller.cli_arguments import CLIArguments
+
+from utils.logger import Logger
 
 
 def main() -> int:
     logger: Logger = Logger(class_name=__name__)
 
+    args: CLIArguments = CLIArguments()
+    args.parse()
+    model_name = args.get("model_name")
+
     try:
 
         logger.info(f"Hello from {__name__}")
-        logger.warning(f"Hello from {__name__}")
-        logger.error(f"Hello from {__name__}")
+        logger.info(f"Model Name: {model_name}")
 
     except Exception as e:
         raise Exception(f"Error Message: {e}")
