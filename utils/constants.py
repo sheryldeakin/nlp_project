5ade1845f028ec8b83ca607275318d1e217f3a16
Henry Rothenberg <hsr205@aol.com>
2025-10-09 16:47:54 -0400

    Alter field member in constants.py module

--- utils/constants.py
+++ utils/constants.py
@@ -1,7 +1,7 @@
 class Constants:
-    COLOR_INFO: str = "\033[97m"  # Bright White
-    COLOR_WARNING: str = "\033[93m"  # Yellow
-    COLOR_ERROR: str = "\033[91m"  # Red
+    COLOR_INFO: str = "\033[60m"
+    COLOR_WARNING: str = "\033[93m"
+    COLOR_ERROR: str = "\033[91m"
     COLOR_RESET: str = "\033[0m"
 
     SLANG_DICT: dict[str, str] = {"brb": "be right back", "idk": "I don't know", "u": "you"}
