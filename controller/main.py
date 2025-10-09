07ca8c83c26ccd626e00b8124941ac6e247e1835
Henry Rothenberg <hsr205@aol.com>
2025-10-09 16:49:35 -0400

    Add demo logic to main.py module

--- controller/main.py
+++ controller/main.py
@@ -1,5 +1,5 @@
 from controller.cli_arguments import CLIArguments
-
+from controller.controller import Controller
 from utils.logger import Logger
 
 
@@ -8,12 +8,17 @@ def main() -> int:
 
     args: CLIArguments = CLIArguments()
     args.parse()
-    model_name = args.get("model_name")
+    model_name_str: str = args.get("model_name")
 
     try:
 
-        logger.info(f"Hello from {__name__}")
-        logger.info(f"Model Name: {model_name}")
+        controller: Controller = Controller()
+
+        logger.info(f"Model Name: {model_name_str}")
+
+        controller.execute_model(model_name_str=model_name_str)
+
+
 
     except Exception as e:
         raise Exception(f"Error Message: {e}")
